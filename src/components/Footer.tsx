31b59e53d8cb748dea49d44c4c8fdfa11f83e09a
chuckerin <chuckerin@gmail.com>
2025-11-10 15:17:48 -0600

    Begining to  add components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer className='footer footer-text'>
+      <p>
+        Created by Chuckerin Black <br></br>© 2024 Copeland Weather App. All
+        rights reserved.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+function Header() {
+  return (
+    <header>
+      <div className='header-text'>Copeland Weather App</div>
+      {/* <table className='header-background'>
+        <tr>
+          <td className='header-logo'>
+            <img src='/copeland.png'></img>
+          </td>
+          <td className='header-text'>Weather App</td>
+        </tr>
+      </table> */}
+    </header>
+  );
+}
+
+export default Header;

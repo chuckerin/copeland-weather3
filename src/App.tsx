8d5f70a106218148bc2c753f9b87d8c68432bb03
chuckerin <chuckerin@gmail.com>
2025-11-10 15:25:06 -0600

    added tab view components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
-import './App.css';
+import { TabView, TabPanel } from 'primereact/tabview';
+import { PrimeIcons } from 'primereact/api';
+
 import Header from './components/Header';
+
 import Footer from './components/Footer';
+import './App.css';
 
 function App() {
+  function handleSubmit(
+    event:
+      | React.FormEvent<HTMLFormElement>
+      | React.MouseEvent<HTMLButtonElement>
+  ) {
+    event.preventDefault();
+  }
+
   return (
     <>
       <Header />
+      <div className='flex justify-content-center tab'>
+        <form onSubmit={handleSubmit}>
+          <TabView className='tab'>
+            <TabPanel
+              header='Zip Code&nbsp;'
+              rightIcon={PrimeIcons.WAVE_PULSE}
+            ></TabPanel>
+            <TabPanel
+              header='City&nbsp;'
+              rightIcon={PrimeIcons.BUILDING}
+            ></TabPanel>
+            <TabPanel
+              header='Geocode&nbsp;'
+              rightIcon={PrimeIcons.GLOBE}
+            ></TabPanel>
+          </TabView>
+        </form>
+      </div>
+      <div className='response-container'></div>
       <Footer />
     </>
   );

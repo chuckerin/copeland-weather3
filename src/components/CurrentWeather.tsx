bceb33296caa59121cd5b2f5c2fc4dc29d008f96
chuckerin <chuckerin@gmail.com>
2025-11-22 13:39:25 -0600

    got fav finally working with a useEffect workaround that isn't the cleanest

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -3,7 +3,7 @@ import type { WeatherItem } from '../utils/types';
 import { Button } from 'primereact/button';
 import { useEffect, useState, type Dispatch, type SetStateAction } from 'react';
 import fetchFavWeather from '../actions/fetch-fav-weather';
-// import setLocalStorage from '../utils/local-storage';
+import setLocalStorage from '../utils/local-storage';
 
 interface Props {
   weatherItem: WeatherItem | null;
@@ -23,40 +23,50 @@ function CurrentWeather(props: Props) {
   const lon = geoCode?.lon;
 
   async function handleLikeClick() {
-    console.log(
-      'hasFav b4',
-      hasFav,
-      'favLocation b4',
-      localStorage.getItem('favLocation'),
-      'city b4',
-      city
-    );
-
     if (hasFav && localStorage.getItem('favLocation') === city) {
-      localStorage.removeItem('favLocation');
+      // localStorage.removeItem('favLocation');
+      localStorage.setItem('favLocation', '');
       setHasFav(false);
+      console.log(
+        'hasFav b4',
+        hasFav,
+        'favLocation b4',
+        localStorage.getItem('favLocation'),
+        'city b4',
+        city
+      );
     } else {
-      localStorage.setItem('favLocation', city ?? '');
-      // await setLocalStorage(city ?? '');
-      setHasFav(true);
+      // localStorage.setItem('favLocation', city ?? '');
+      setHasFav(false);
+      // console.log(
+      //   'hasFav b4',
+      //   hasFav,
+      //   'favLocation b4',
+      //   localStorage.getItem('favLocation'),
+      //   'city b4',
+      //   city
+      // );
+      await setLocalStorage(city ?? '').then(() => {
+        setHasFav(true);
+        // console.log(
+        //   'hasFav aft',
+        //   hasFav,
+        //   'favLocation aftr',
+        //   localStorage.getItem('favLocation'),
+        //   'city after',
+        //   city
+        // );
+      });
     }
-
-    console.log(
-      'hasFav aft',
-      hasFav,
-      'favLocation aftr',
-      localStorage.getItem('favLocation'),
-      'city after',
-      city
-    );
   }
 
   useEffect(() => {
     // This code runs only once after the component mounts
     // console.log('MyComponent has mounted!');
     const favCity = localStorage.getItem('favLocation');
+    if (favCity === null) return;
     // console.log('favCity', favCity);
-    if (favCity !== null) {
+    if (favCity !== '') {
       setHasFav(true);
       fetchFavWeather(favCity).then((response) => {
         if (!response) return;
@@ -74,6 +84,10 @@ function CurrentWeather(props: Props) {
     }
     // You can perform data fetching, set up subscriptions, etc. here
   }, []); // Empty dependency array ensures it runs only on mount
+
+  // setState will not update for new fav cities without this useEffect
+  // it's hacky but works
+  useEffect(() => {}, [hasFav]);
 
   return (
     <>
--- src/utils/local-storage.ts
+++ src/utils/local-storage.ts
@@ -1,7 +1,7 @@
 async function setLocalStorage(city: string) {
   localStorage.setItem('favLocation', city ?? '');
   // this is bullshit, but the icon is not refreshing on a new fav location being set
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 0));
 }
 
 export default setLocalStorage;

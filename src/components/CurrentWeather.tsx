c02967b41a39c21aa2933462a807f424e18c5e95
chuckerin <chuckerin@gmail.com>
2025-11-24 11:41:23 -0600

    testing the deploy

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -79,8 +79,8 @@ function CurrentWeather(props: Props) {
                 id='fav'
                 icon={
                   hasFav && localStorage.getItem('favLocation') === city
-                    ? 'pi pi-heart-fill pi-spin btn-fill'
-                    : 'pi pi-heart pi-spin btn-fill'
+                    ? 'pi pi-apple-fill pi-spin btn-fill'
+                    : 'pi pi-apple pi-spin btn-fill'
                 }
                 rounded
                 text
